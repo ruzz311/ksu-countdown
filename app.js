00daf8938af9193998fef72ff6edd23caac97c3d
Russell Madsen <ruzz311@gmail.com>
2012-10-18 03:50:09 -0500

    removing stupid nodester stuff and adding nodejitsu stuff

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var http = require('http'),
     ss = require('socketstream'),
-    port = listen(process.env['app_port'] || 3000);
+    port = process.env.PORT || 3000;
 
 // Define a single-page client called 'main'
 ss.client.define('main', {
